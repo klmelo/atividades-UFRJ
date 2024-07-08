201680b8e97c245674d51226aa4e6ab983ec9dc4
K. L. Melo <165231359+klmelo@users.noreply.github.com>
2024-07-08 02:57:47 +0000

    Create main.c

---
+++ Programação de Computadores I/Lista 3/a8/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+
+typedef struct {
+    int idade;
+    float peso;
+    char nome[MAX];
+}Pessoa;
+
+int busca(int j, Pessoa v[], int x) {
+    int inicio = 0;
+    int fim = j - 1; 
+
+    while (inicio <= fim) {
+        int meio = (inicio + fim) / 2;
+        if (v[meio].peso == x) {
+            return meio; 
+        } else if (v[meio].peso > x) {
+            fim = meio - 1; 
+        } else {
+            inicio = meio + 1; 
+        }
+    } return -1;
+}
+int main(void){
+    int j;
+    printf("digite a quantidade de pessoas\n");
+    scanf("%d", &j);
+    Pessoa lista[j];
+
+//Obtendo os dados das pessoas
+
+    for(int i = 0; i < j; i++){
+	printf("Digite a idade da pessoa %d \n", i);
+	scanf("%d", &lista[i].idade);
+	printf("Digite o peso da pessoa %d \n", i);
+	scanf("%f", &lista[i].peso);
+	printf("Digite o nome da pessoa %d .\n", i);
+	scanf("%s", lista[i].nome);
+    }
+
+//Ordenando as pessoas por idade
+    int trocou = 1;
+    for (int k = j-1 ; k>0 && trocou; k--) {
+	trocou = 0;
+	for (int i = 0; i < k; i++) {
+	    //se trocar o "<" por ">" vira ordem crescente
+	    if (lista[i].idade > lista[i+1].idade) {
+	    int aux = lista[i+1].idade;
+	    lista[i+1].idade = lista[i].idade;
+	    lista[i].idade = aux;
+	    trocou = 1;
+	    }
+	}
+    }
+
+//imprime o vetor lista;
+
+    for (int i = 0; i < j; i++) {
+	printf("A idade de %d é %d\n", i, lista[i].idade);
+    }
+    
+//Parte 2 da questão
+    
+    printf("Agora digite um peso qualquer \n");
+    int pesoU;
+    scanf("%d", &pesoU);
+    int resposta = busca(j, lista, pesoU);
+    if (resposta == -1) {
+	printf("Não há ninguém aqui com esse peso\n");
+	return 0;
+    }
+    else puts(lista[resposta].nome);
+    return 0;
+    
+}
