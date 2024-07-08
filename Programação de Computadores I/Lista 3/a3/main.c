08e1b56fb2931c29f8e73af7e674a189d87184bd
K. L. Melo <165231359+klmelo@users.noreply.github.com>
2024-07-08 02:37:37 +0000

    Create main.c

---
+++ Programação de Computadores I/Lista 3/a3/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+int procuraPalavra(char texto[], char palavra[]);
+int main(void) {
+    char texto[MAX];
+    char palavra[MAX];
+    printf("Digite o texto\n");
+    fgets(texto, MAX, stdin);
+    printf("Digite a palavra\n");
+    fgets(palavra, MAX, stdin);
+    if (palavra[strlen(palavra)-1] == '\n') {
+	palavra[strlen(palavra)-1] = '\0';
+    }
+    printf("A palavra '%s' está no texto e o seu índice é %d \n", palavra, procuraPalavra(texto, palavra));
+    
+    return 0;
+}
+
+int procuraPalavra(char texto[], char palavra[]) {
+    int tamanhodotexto = strlen(texto);
+    int tamanhodapalavra = strlen(palavra);
+    int aux=0;
+
+    for (int i=0; i<= tamanhodotexto; i++) {
+	if (texto[i] == palavra [aux]) {
+	    for( ; aux <= tamanhodapalavra; aux++, i++) {
+		if(texto[i] != palavra[aux]) {
+		    break;
+		}
+	    } if ((texto[i] == palavra[tamanhodapalavra])) {
+		return i - tamanhodapalavra;
+	    }
+	}
+    } return -1;
+}
